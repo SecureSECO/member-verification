d8fbf7eb1d02563f1efd3fd539cc50669ef36d97
xray27 <wouter.beffers@gmail.com>
2023-03-14 12:37:18 +0100

    Added mock test

--- web/src/App.test.tsx
+++ web/src/App.test.tsx
@@ -3,6 +3,6 @@ import { render, screen } from '@testing-library/react';
 import App from './App';
 
 it('renders title', () => {
-  render(App());
+  render(<App/>);
   expect(screen.getByText('SecureSECO Verification')).toBeInTheDocument();
 });
