fe8a9a597adce56fdb816aeb39ea483d117571f9
Mike <23509561+mik-0@users.noreply.github.com>
2023-04-10 13:56:51 +0200

    Add license to files

--- block/test/GithubVerification.js
+++ block/test/GithubVerification.js
@@ -1,3 +1,11 @@
+/**
+  * This program has been developed by students from the bachelor Computer Science at Utrecht University within the Software Project course.
+  * © Copyright Utrecht University (Department of Information and Computing Sciences)
+  *
+  * This source code is licensed under the MIT license found in the
+  * LICENSE file in the root directory of this source tree.
+  */
+
 // TODO: write separate getStamps function to avoid duplicate code
 const GithubVerification = artifacts.require("GithubVerification");
 
