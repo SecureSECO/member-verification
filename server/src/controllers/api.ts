6f84e167d0722166669f4a867f2ff8012aeb0e67
Mike <23509561+mik-0@users.noreply.github.com>
2023-06-05 11:38:57 +0200

    fix: callback url

--- server/src/controllers/api.ts
+++ server/src/controllers/api.ts
@@ -124,7 +124,7 @@ const generateProof = async (
 /// GITHUB OAUTH
 const REDIRECT_URI =
     config.NODE_ENV === "production"
-        ? "https://securesecodao.science.uu.nl/verification_api/github_callback"
+        ? "https://securesecodao-api.herokuapp.com/verification_api/github_callback"
         : "http://localhost:43210/api/github_callback";
 export const authorize = async (req: Request, res: Response): Promise<void> => {
     try {
